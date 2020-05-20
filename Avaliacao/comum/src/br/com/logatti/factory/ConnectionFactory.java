f1523da8ea1bc6047b7d189f5c0684997687f885
daviguelfi <daviguelfi@gmail.com>
2020-05-20 10:42:30 -0300

    create a connection with sqlite

--- Avaliacao/comum/src/br/com/logatti/factory/ConnectionFactory.java
+++ Avaliacao/comum/src/br/com/logatti/factory/ConnectionFactory.java
@@ -5,9 +5,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-    public Connection getConnection() {
+    public Connection getConnection() throws ClassNotFoundException {
         try {
-            return DriverManager.getConnection("jdbc:mysql://localhost/teste", "root", "root");
+//            return DriverManager.getConnection("jdbc:mysql://localhost/teste", "root", "root");
+            Class.forName("org.sqlite.JDBC");
+            return DriverManager.getConnection("jdbc:sqlite:/home/davi/Área de Trabalho/workspace/logatti/logatti/Avaliacao/loja.db");
         } catch (SQLException excecao) {
             throw new RuntimeException(excecao);
         }
